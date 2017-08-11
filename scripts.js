4cdb17d15a5b2099a6a15c4957e0b84c709924f0
manuel garcia <manueldjg@ymail.com>
2017-08-11 09:21:42 -0400

    got turn functionality to work

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-let playerOne = [];
-let playerTwo = [];
-let playTurn = 0;
+let playerOne = {playTurnId: 1};
+let playerTwo = {playTurnId: 2};
+let currentPlayer;
 let playerOneMoveCount = [];
 let playerTwoMoveCount = [];
 let circles = $('div.r1, div.r2, div.r3, div.r4, div.r5, div.r6, div.r7');
@@ -10,81 +10,35 @@ let circles = $('div.r1, div.r2, div.r3, div.r4, div.r5, div.r6, div.r7');
 $('button#start').click(function(){
     getPlayerOneName();
     getPlayerTwoName();
+    currentPlayer = playerOne;
 });
 function getPlayerOneName(){
     let nameInput = prompt('Enter first players name!');
-    playerOne.push(nameInput)
+    playerOne.name = nameInput;
 }
 function getPlayerTwoName(){
   let nameInput = prompt('Enter second players name!');
-  playerTwo.push(nameInput)
+  playerTwo.name = nameInput;
+}
+
+function clickAssign(){
+    $(circles).click(playerClickCell)
+
 }
 // when a circle is clicked, a color is populated.
 //need it to populate, appropriate player color
 
-
-// function letThemPlay() {
-//   $(circles).click(function() {
-//     playerOneMoves();
-//     playerTwoMoves();
-//   })
-// }
-// letThemPlay();
-
-function playerOneMoves() {
-    let circles = $('div.r1, div.r2, div.r3, div.r4, div.r5, div.r6, div.r7');
-        $(circles).click(function(){
-            $(this).css('background-color', 'red');
-            console.log('click')
-        })
+function playerClickCell(){
+    console.log(this)
+  if (currentPlayer.playTurnId === 1) {
+    $(this).css('background-color', 'red')
+    currentPlayer = playerTwo
+  } else {
+    $(this).css('background-color', 'blue')
+    currentPlayer = playerOne
+  }
 }
-playerOneMoves();
 
-function playerTwoMoves() {
-  let circles = $('div.r1, div.r2, div.r3, div.r4, div.r5, div.r6, div.r7');
-  $(circles).click(function(){
-    $(this).css('background-color', 'blue');
-    console.log('click')
-  })
-}
-// playerTwoMoves();
-
-//trying to make sure when a circle is clicked
-//that the correct color is assigned and the correct
-//player is recognized
-
-// function currentPlayer(){
-//     turnCount();
-//     if (playTurn % 2 === 0){
-//         currentPlayer.push(turnCount[i])
-//     }   else {
-//         currentPlayer.push(turnCount[i])
-//     }
-// }
-//
-function turnCount(){
-  playTurn++;
-}
-//
-//
-//
-//
-// function playerMove() {
-//   turnCount();
-//   if (playTurn % 2 === 0){
-//     clickAssign();
-//   } else {
-//     clickAssign()
-//   }
-//   if (playTurn > 42){
-//     var conf = confirm('Cats Game, play again?');
-//     if(conf){
-//       resetBoard();
-//     }
-//   }
-// }
-//
-// playerMove()
 
 // $('div.r1.c1, div.r2.c1').click(function(){
 //     $(this).css('background-color','red');
